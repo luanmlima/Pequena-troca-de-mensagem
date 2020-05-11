cf0d49c2a58966a7c75124f04019f6ebc01cb7f6
Luan Martins <luan.lima366@gmail.com>
2020-05-11 10:42:11 -0300

    Projeto simples de troca de mensagens

---
+++ ConsoleApp1/Entities/Post.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Entities
+{
+    class Post
+    {
+        public DateTime Momment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime momment, string title, string content, int likes)
+        {
+            Momment = momment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+            
+        }
+
+        public void AddComment(Comment comments)
+        {
+            Comments.Add(comments);
+        }
+        public void RemoveComment(Comment comments)
+        {
+            Comments.Remove(comments);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Momment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
